a30b880d5539dd89f9066c8f0313e86582965330
zengwangfa <zengwangfa@outlook.com>
2020-03-30 00:21:33 +0800

    :hammmer: Change: 优化实现方法：CPU、硬盘、内存、网卡网速状态

--- drivers/cpu_status.c
+++ drivers/cpu_status.c
@@ -9,11 +9,9 @@
 #include <elog.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <math.h>
-
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -21,8 +19,6 @@
 #include <fcntl.h>
 
 #define TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"
-#define MAX_SIZE 32
-
 
 
  /**
@@ -31,105 +27,201 @@
  */
 float get_cpu_temp(void)
 {
-    int fd;
-    float temp = 0;
-    char buf[MAX_SIZE];
-    // 打开/sys/class/thermal/thermal_zone0/temp
-    fd = open(TEMP_PATH, O_RDONLY);
-    if (fd < 0)
-    {
-        fprintf(stderr, "failed to open thermal_zone0/temp\n");
-        return -1;
-    }
-    // 读取内容
-    if (read(fd, buf, MAX_SIZE) < 0)
-    {
-        fprintf(stderr, "failed to read temp\n");
-        return -1;
-    }
-    // 转换为浮点数打印
-    temp = atoi(buf) / 1000.0;
-    // 关闭文件
-    close(fd);
+    float temp;
+    static FILE *fp;
 
+    // thermal_zone0 表示 CPU0
+    fp = fopen(TEMP_PATH, "r");
+
+    fscanf(fp,"%f", &temp); // 读取第1行 cpu温度数据
+
+    temp /= 1000.0f;
+
+    fclose(fp);
     return temp;
 }
 
  /**
  * @brief  获取 内存情况
- * @return 内存使用状况 结构体指针
+ * @param  memory_t 结构体指针
  */
-memory_t *get_memory_status(void)
+void get_memory_status(memory_t *memory)
 {
-    static FILE *fd;
-    static char buff[50];
-    static char name1[20]; // 用于保存 内存名称(eg. total/available)
-    static char name2[20]; // 用于保存 单位    (eg. kB)
+    FILE *fp;
+    char name1[20]; // 用于保存 内存名称(eg. total/available)
+    char name2[20]; // 用于保存 单位    (eg. kB)
 
-    static memory_t memory_dev;
-    static memory_t *memory = &memory_dev;
+    fp = fopen("/proc/meminfo", "r");
 
-    fd = fopen("/proc/meminfo", "r");
-
-    fgets(buff, sizeof(buff), fd); // 读取第1行 total
-    sscanf(buff, "%s %u %s", name1, &memory->total, name2);
-    // printf("%s %u %s\n", name1, memory->total, name2); // 用于调试打印
-
-    fgets(buff, sizeof(buff), fd); // 跳过第2行 free
-
-    fgets(buff, sizeof(buff), fd); // 读取第3行 available
-    sscanf(buff, "%s %u %s", name1, &memory->available, name2);
-    //printf("%s %u %s\n", name1, memory->available, name2);
+    fscanf(fp,"%s %u %s", name1, &memory->total, name2); // 读取第1行 total
+    fscanf(fp,"%s %u %s", name1, &memory->free, name2);  // 读取第2行 free
+    fscanf(fp,"%s %u %s", name1, &memory->available, name2); // 读取第3行 available
 
     memory->usage_rate = (float)(memory->total - memory->available) / memory->total * 100.0f;
-    // printf("mem usage_rate %f%% \n", memory->usage_rate);
 
-    fclose(fd); // 关闭文件
-
-    return memory;
+    fclose(fp); 
 }
 
-// 获取 cpu 使用情况
-void get_cpuoccupy(CPU_OCCUPY *cpust)
+ /**
+ * @brief  获取 磁盘状态
+ * @param  disk_t 结构体指针
+ * @notice 在shell下输入 df 命令可以进行对照
+ */
+void get_disk_status(disk_t *disk)
 {
-    FILE *fd;
-    char buff[256];
-    CPU_OCCUPY *cpu_occupy;
-    cpu_occupy = cpust;
+	FILE *fp;
+	float b, c; 
+    // 用于保存各个文件系统的 总容量大小、已使用大小    
+    float disk_total = 0, disk_used = 0; 
+	char  a[20], d[20], e[20], f[20], buf[100];
 
-    fd = fopen("/proc/stat", "r");
-    fgets(buff, sizeof(buff), fd);
+	fp = popen("df", "r");
+	fgets(buf, sizeof(buf), fp); // 读取第1行描述信息(相当于跳过第1行)
 
-    sscanf(buff, "%s %u %u %u %u", cpu_occupy->name, &cpu_occupy->user, &cpu_occupy->nice, &cpu_occupy->system, &cpu_occupy->idle);
+    /*  eg.
+        Filesystem     1K-blocks   Used Available Use% Mounted on
+        udev               85324      0     85324   0% /dev
+    */
+	while (fscanf(fp, "%s %f %f %s %s %s", a, &b, &c, d, e, f) > 0) {
+        disk_total += b;
+		disk_used  += c;
+	}
 
-    fclose(fd);
+	disk->total      = disk_total / 1024; // 转换为Mb单位
+	disk->available  = (disk_total - disk_used) / 1024;
+	disk->usage_rate = disk_used / disk_total * 100;
+
+	pclose(fp);
 }
 
-// 获取 5s 内的 cpu 使用率
+
+ /**
+ * @brief  获取 cpu总时间
+ * @param  cpuInfo_t 结构体指针
+ * @retval uint32_t cpu总时间
+ */
+uint32_t get_cpuTotalTime(cpuInfo_t *cpuInfo)
+{
+	return (cpuInfo->user + cpuInfo->nice   + cpuInfo->system +
+            cpuInfo->idle + cpuInfo->iowait + cpuInfo->irq    +
+            cpuInfo->softirq);
+}
+
+ /**
+ * @brief  获取 cpu状态信息
+ * @param  cpuInfo_t 结构体指针
+ */
+void get_cpuInfo(cpuInfo_t *cpuInfo)
+{
+	FILE *fp;
+
+    fp = fopen("/proc/stat", "r");
+    
+	fscanf(fp, "%s %u %u %u %u %u %u %u",
+	       cpuInfo->name, 
+           &cpuInfo->user, &cpuInfo->nice,
+	       &cpuInfo->system, &cpuInfo->idle, 
+           &cpuInfo->iowait, &cpuInfo->irq, &cpuInfo->softirq);
+
+	fclose(fp);
+}
+
+ /**
+ * @brief  获取1s内的cpu使用率
+ * @retval float cpu使用率
+ */
 float get_cpu_usage(void)
 {
-    CPU_OCCUPY cpu_stat1;
-    CPU_OCCUPY cpu_stat2;
+    float cpu_usage;
+    uint32_t idle_diff;  // 两次空闲时间的差值
+    uint32_t total_diff; // 两次总时间的差值
 
-    //第一次获取cpu使用情况
-    get_cpuoccupy(&cpu_stat1);
-    sleep(5);
-    //第二次获取cpu使用情况
-    get_cpuoccupy(&cpu_stat2);
+    cpuInfo_t cpuInfo1;
+    cpuInfo_t cpuInfo2;
 
-    unsigned long od, nd;
-    unsigned long id, sd;
-    float cpu_use = 0;
+    /* 注解:
+    * CPU总时间 T_total = user+system+nice+idle+iowait+irq+softirq
+    * CPU在t1到t2时间段总的使用时间 total_diff = T_total2 - T_total1
+    * CPU在t1到t2时间段空闲使用时间 idle_diff  = T_idle2  - T_idle1 
+    * CPU在t1到t2时间段即时利用率 cpu_usage = 1 - CPU空闲使用时间 / CPU总的使用时间
+    */
 
-    od = (unsigned long)(cpu_stat1.user + cpu_stat1.nice + cpu_stat1.system + cpu_stat1.idle); //第一次(用户+优先级+系统+空闲)的时间再赋给od
-    nd = (unsigned long)(cpu_stat2.user + cpu_stat2.nice + cpu_stat2.system + cpu_stat2.idle); //第二次(用户+优先级+系统+空闲)的时间再赋给od
+    // 第1次获取cpu使用情况
+    get_cpuInfo(&cpuInfo1);
 
-    id = (unsigned long)(cpu_stat2.user - cpu_stat1.user);     // 用户第一次和第二次的时间之差再赋给id
-    sd = (unsigned long)(cpu_stat2.system - cpu_stat1.system); // 系统第一次和第二次的时间之差再赋给sd
-    if ((nd - od) != 0)
-        cpu_use = (float)((sd + id) * 100) / (nd - od); //((用户+系统)乖100)除(第一次和第二次的时间差)再赋给g_cpu_used
-    else
-        cpu_use = 0;
+    sleep(1); // 休眠1s
 
-    return cpu_use;
+    // 第2次获取cpu使用情况
+    get_cpuInfo(&cpuInfo2);
+
+    idle_diff  = cpuInfo2.idle - cpuInfo1.idle;
+    total_diff = get_cpuTotalTime(&cpuInfo2) - get_cpuTotalTime(&cpuInfo1);
+    
+	cpu_usage = (float)(total_diff - idle_diff) * 100.0f / total_diff;
+    return cpu_usage;
 }
+
+
+/**
+ * @brief  获取对应网卡的网络数据
+ * @param  
+ *  netData_t 网络数据结构体指针
+ *  eth 网卡名
+ */
+void get_net_data(netData_t *net_data, char *eth)
+{
+
+	FILE *fp;
+	char name[20];
+	uint64_t rb, rp, re, rd, rfi, rfr, rc, rm,
+	    tb, tp, te, td, tfi, tfr, tc, tm;
+
+	bzero(net_data, sizeof(netData_t));
+
+	fp = fopen("/proc/net/dev", "r");
+	while (fscanf(fp,
+		"%s %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+		name, &rb, &rp, &re, &rd, &rfi, &rfr, &rc, &rm, &tb, &tp,
+		&te, &td, &tfi, &tfr, &tc, &tm) > 0) {
+
+		if (strcmp(name, eth) == 0) {
+
+			net_data->rb = rb;
+			net_data->rp = rp;
+			net_data->tb = tb;
+			net_data->tp = tp;
+			break;
+		}
+
+	}
+}
+
+ /**
+ * @brief  获取对应网卡的网络数据
+ * @param  网卡名
+ * @notice 注意：这里进行了单位转换，转换为 kb/Mb(而不是 kB/MB)
+ */
+char *get_net_speed(char *eth)
+{
+    float ns;
+	char ethc[20];
+    netData_t nd1, nd2;
+	static char netspeed[15];
+
+    sprintf(ethc, "%s:", eth); // 加上:
+
+	get_net_data(&nd1, ethc);
+	sleep(1);
+	get_net_data(&nd2, ethc);
+
+	ns = (float)(((nd2).rb + nd2.tb) - (nd1.rb + nd1.tb)) / 1024 * 8; // 转换为 kbps/Mbps
+
+    if(ns < 1024)
+        sprintf(netspeed, "%.2f kb/s", ns);
+    else
+        sprintf(netspeed, "%.2f Mb/s", ns / 1024); 
+
+	return netspeed;
+}
+
+
--- drivers/cpu_status.h
+++ drivers/cpu_status.h
@@ -9,27 +9,54 @@
 
 typedef struct // 定义一个cpu occupy的结构体
 {
-    char name[20];       // 
-    unsigned int user;   // 在用户模式下花费的时间
-    unsigned int nice;   // 在低优先级用户模式下花费的时间
-    unsigned int system; // 在系统模式下花费的时间
-    unsigned int idle;   // 等待任务花费的时间
-} CPU_OCCUPY;
+    char name[8];      
+    uint32_t user;   // 用户态时间 单位：jiffies (1 jiffies = 10ms)
+    uint32_t nice;   // 用户态时间(低优先级，nice>0)
+    uint32_t system; // 内核态时间
+    uint32_t idle;   // 空闲时间
+    uint32_t iowait; // I/O等待时间
+    uint32_t irq;    // 硬中断
+    uint32_t softirq;// 软中断
+} cpuInfo_t;
 
-/* 内存使用状况 描述符 */
+
+/* 内存状况 描述符 */
 typedef struct 
 {
     uint32_t total;     // 内存总数
-    uint32_t available; // 可用内存数 MemAvailable≈MemFree+Buffers+Cached
+    uint32_t free;      // 完全未用到的物理内存
+    uint32_t available; // 可用内存数 MemAvailable ≈ MemFree + Buffers(文件缓冲大小) + Cached(高速缓冲大小)
     float    usage_rate;// 使用率
 
 } memory_t;
 
-memory_t *get_memory_status(void); // 获取 内存情况
 
-float get_cpu_temp(void);            // 获取 CPU 温度
+/* 硬盘状况 描述符 */
+typedef struct
+{
+	uint32_t total;
+    uint32_t available;
+    float    usage_rate;
+} disk_t;
 
 
-float get_cpu_usage(void);           // 获取 5s 内的 cpu 使用率
+/* 网络数据 描述符 */
+typedef struct  
+{
+	uint64_t rp, rb;
+	uint64_t tp, tb;
+} netData_t;
+
+
+
+float get_cpu_temp(void); // 获取 CPU 温度
+float get_cpu_usage(void);// 获取 1s 内的 cpu 使用率
+
+void get_disk_status(disk_t *disk);	
+void get_memory_status(memory_t *memory); // 获取 内存情况
+
+char *get_net_speed(char *eth);
+
+
 
 #endif /* _DRV_CPU_TEMP_H_ */
